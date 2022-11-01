b65518be5ded9d916b7ff355cf8685c2501d9c17
Mallem Houda <houda.mallem@gmail.com>
2022-11-01 08:37:56 +0100

    Update renderers.py

--- api/utils/renderers.py
+++ api/utils/renderers.py
@@ -4,7 +4,7 @@ from rest_framework.renderers import JSONRenderer
 
 
 class CustomJSONRenderer(JSONRenderer):
-    """ a custom json render class to render the response in the needed format  """
+    """ a custom json render class to render the response in the right format  """
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
         response_data = {"errors": {}, "data": [], 'date': datetime.now()}
